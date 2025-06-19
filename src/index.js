bed96a0af180b48db61679a8eaf73d57b0d83105
Sam <samroux808@gmail.com>
2025-06-18 23:02:08 -0500

    Refined data collection to target high-entropy values

--- src/index.js
+++ src/index.js
@@ -12,66 +12,26 @@ export default class Snatch {
      * This method collects various properties from the `navigator` object,
      */
     snatch() {
-        this.dataset["platform"] = navigator.platform;
-        this.dataset["userAgent"] = navigator.userAgent;
-        this.dataset["language"] = navigator.language;
-        this.dataset["languages"] = navigator.languages;
-        this.dataset["vendor"] = navigator.vendor;
-        this.dataset["appVersion"] = navigator.appVersion;
-        this.dataset["appName"] = navigator.appName;
-        this.dataset["appCodeName"] = navigator.appCodeName;
-        this.dataset["product"] = navigator.product;
-        this.dataset["productSub"] = navigator.productSub;
-        this.dataset["hardwareConcurrency"] = navigator.hardwareConcurrency;
-        this.dataset["deviceMemory"] = navigator.deviceMemory;
-        this.dataset["cookieEnabled"] = navigator.cookieEnabled;
-        this.dataset["doNotTrack"] = navigator.doNotTrack;
-        this.dataset["maxTouchPoints"] = navigator.maxTouchPoints;
-        this.dataset["connection"] = navigator.connection ? {
-            effectiveType: navigator.connection.effectiveType,
-            downlink: navigator.connection.downlink,
-            rtt: navigator.connection.rtt,
-            saveData: navigator.connection.saveData
-        } : null;
-        this.dataset["geolocation"] = "geolocation" in navigator;
-        this.dataset["mediaDevices"] = "mediaDevices" in navigator;
-        this.dataset["permissions"] = "permissions" in navigator;
-        this.dataset["storage"] = "storage" in navigator;
-        this.dataset["serviceWorker"] = "serviceWorker" in navigator;
-        this.dataset["webGL"] = (function() {
-            try {
-                const canvas = document.createElement('canvas');
-                const gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
-                return gl ? {
-                    vendor: gl.getParameter(gl.VENDOR),
-                    renderer: gl.getParameter(gl.RENDERER),
-                    version: gl.getParameter(gl.VERSION),
-                    shadingLanguageVersion: gl.getParameter(gl.SHADING_LANGUAGE_VERSION)
-                } : null;
-            } catch (e) {
-                return null;
-            }
-        })();
-        this.dataset["canvas"] = (function() {
-            try {
-                const canvas = document.createElement('canvas');
-                const ctx = canvas.getContext('2d');
-                ctx.fillStyle = 'rgba(255, 0, 0, 0.5)';
-                ctx.fillRect(0, 0, 50, 50);
-                return canvas.toDataURL();
-            } catch (e) {
-                return null;
-            }
-        })();
-        this.dataset["fonts"] = (async function() {
-            try {
-                const fonts = await document.fonts.ready;
-                return Array.from(fonts).map(font => font.family);
-            } catch (e) {
-                return [];
-            }
-        })();
-        this.dataset["screen"] = {
+        this.dataset.userAgent = navigator.userAgent;
+        this.dataset.platform = navigator.platform;
+        this.dataset.timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+        this.dataset.language = navigator.language || navigator.userLanguage;
+        this.dataset.languages = navigator.languages;
+        this.dataset.cookieEnabled = navigator.cookieEnabled;
+        this.dataset.doNotTrack = navigator.doNotTrack || window.doNotTrack || navigator.msDoNotTrack;
+        this.dataset.hardwareConcurrency = navigator.hardwareConcurrency;
+        this.dataset.deviceMemory = navigator.deviceMemory || "unknown";
+        this.dataset.plugins = Array.from(navigator.plugins).map(plugin => ({
+            name: plugin.name,
+            version: plugin.version,
+            description: plugin.description
+        }));
+        this.dataset.mimeTypes = Array.from(navigator.mimeTypes).map(mimeType => ({
+            type: mimeType.type,
+            suffixes: mimeType.suffixes,
+            description: mimeType.description
+        }));
+        this.dataset.screen = {
             width: screen.width,
             height: screen.height,
             colorDepth: screen.colorDepth,
@@ -80,37 +40,17 @@ export default class Snatch {
                 type: screen.orientation.type,
                 angle: screen.orientation.angle
             } : null
-        };
-        this.dataset["timezone"] = Intl.DateTimeFormat().resolvedOptions().timeZone;
-        this.dataset["plugins"] = Array.from(navigator.plugins).map(plugin => ({
-            name: plugin.name,
-            description: plugin.description,
-            filename: plugin.filename,
-            length: plugin.length
-        }));
-        this.dataset["mimeTypes"] = Array.from(navigator.mimeTypes).map(mimeType => ({
-            type: mimeType.type,
-            suffixes: mimeType.suffixes,
-            description: mimeType.description
-        }));
-        this.dataset["oscpu"] = navigator.oscpu || null;
-        this.dataset["audio"] = (function() {
-            try {
-                const audio = new Audio();
-                return {
-                    codecSupport: {
-                        codec_wav: audio.canPlayType('audio/wav; codecs="1"'),
-                        codec_mp3: audio.canPlayType('audio/mpeg; codecs="mp3"'),
-                        codec_vorbis: audio.canPlayType('audio/ogg; codecs="vorbis"'),
-                        codec_opus: audio.canPlayType('audio/ogg; codecs="opus"'),
-                        codec_flac: audio.canPlayType('audio/ogg; codecs="flac"'),
-                    },
-                    sampleRate: audio.sampleRate || null
-                };
-            } catch (e) {
-                return null;
-            }
-        })();
+        }
+        navigator.userAgentData.getHighEntropyValues([
+            "architecture",
+            "model",
+            "platform",
+            "platformVersion",
+            "uaFullVersion",
+            "bitness"
+        ]).then(function(highEntropyValues) {
+            this.dataset.highEntropyValues = highEntropyValues;
+        });
     }
 
     /**
