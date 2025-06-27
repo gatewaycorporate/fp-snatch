df861120a1e4c798a9bf7edd947bb6c98bc0b2b9
Sam <samroux808@gmail.com>
2025-06-27 14:08:50 -0500

    Added font detection

--- src/index.js
+++ src/index.js
@@ -58,6 +58,9 @@ export default class Snatch {
             } : null
         }
 
+        // Collect font information
+        this.dataset.fonts = this.detectInstalledFonts();
+
         // Collect High Entropy Values
         try {
             var highEntropyValues = {};
@@ -78,6 +81,68 @@ export default class Snatch {
             console.warn("High Entropy Values not available:", error);
             this.dataset.highEntropyValues = {};
         }
+    }
+
+    detectInstalledFonts() {
+        const fontList = [
+            "Arial", "Helvetica", "Times New Roman", "Courier New", "Verdana", "Georgia",
+            "Palatino", "Garamond", "Bookman", "Comic Sans MS", "Trebuchet MS", "Arial Black",
+            "Impact", "Lucida Console", "Lucida Sans Unicode", "Tahoma", "Century Gothic",
+            "Calibri", "Cambria", "Candara", "Consolas", "Corbel", "Constantia", "Franklin Gothic Medium",
+            "Gill Sans", "Futura", "Baskerville", "Didot", "Rockwell", "Bodoni", "Century Schoolbook",
+            "American Typewriter", "Andale Mono", "Optima", "Segoe UI", "Source Sans Pro",
+            "PT Sans", "Raleway", "Lato", "Roboto", "Open Sans", "Montserrat", "Ubuntu", "Noto Sans",
+            "Avenir", "Proxima Nova", "Museo Sans", "Merriweather", "Droid Sans", "Playfair Display",
+            "Josefin Sans", "Oswald", "Lobster", "Quicksand", "Karla", "Work Sans", "Inter", "Cabin",
+            "Arvo", "Zilla Slab", "Nunito", "Exo", "Rubik", "Muli", "Oxygen", "Inconsolata", "Anton",
+            "Pacifico", "Alegreya", "Cormorant", "EB Garamond", "Asap", "Titillium Web", "Varela Round",
+            "Abel", "Barlow", "Righteous", "Archivo", "Crimson Text", "Manrope", "Poppins", "Chivo",
+            "Teko", "Hind", "Baloo", "Heebo", "Fjalla One", "Maven Pro", "Catamaran", "Yanone Kaffeesatz",
+            "PT Serif", "Space Grotesk", "IBM Plex Sans", "Signika", "Questrial", "Bangers",
+            "Dancing Script", "Fredoka", "Press Start 2P", "Shadows Into Light"
+        ];
+
+        const baseFonts = ['monospace', 'sans-serif', 'serif'];
+        const testString = 'mmmmmmmmmmlli';
+        const testSize = '72px';
+        const detected = [];
+
+        const body = document.body || document.getElementsByTagName('body')[0];
+        const span = document.createElement('span');
+        span.style.fontSize = testSize;
+        span.innerHTML = testString;
+        span.style.position = 'absolute';
+        span.style.left = '-9999px';
+        span.style.visibility = 'hidden';
+
+        const defaultDims = {};
+        for (const base of baseFonts) {
+            span.style.fontFamily = base;
+            body.appendChild(span);
+            defaultDims[base] = { width: span.offsetWidth, height: span.offsetHeight };
+            body.removeChild(span);
+        }
+
+        for (const font of fontList) {
+            let detectedFont = false;
+            for (const base of baseFonts) {
+                span.style.fontFamily = `'${font}',${base}`;
+                body.appendChild(span);
+                const width = span.offsetWidth;
+                const height = span.offsetHeight;
+                body.removeChild(span);
+
+                if (width !== defaultDims[base].width || height !== defaultDims[base].height) {
+                    detectedFont = true;
+                    break;
+                }
+            }
+            if (detectedFont) {
+                detected.push(font);
+            }
+        }
+
+        return detected;
     }
 
     /**
